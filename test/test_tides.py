b5588deeb7610e4542f8cbcd6d7da8e93af28f75
mattpurdy352 <bns514@york.ac.uk>
2025-05-24 01:08:37 +0100

    timezone test

--- test/test_tides.py
+++ test/test_tides.py
@@ -32,7 +32,33 @@ class TestTidalAnalysis():
             tidal_analysis_module._check_utide_availability() # Call using the module alias
         except EnvironmentError:
             pytest.fail("EnvironmentError was raised even though utide components were mocked as available.")
-    
+
+
+    def test_prepare_inputs_same_timezone(self):
+        """
+        Tests basic functionality when data.index and start_datetime_epoch 
+        are already in the same timezone (e.g. UTC).
+        """
+        utc_tz = pytz.utc
+        data_timestamps_utc = pd.to_datetime([
+            '2023-01-01 00:00:00',
+            '2023-01-01 01:00:00',
+        ]).tz_localize(utc_tz)
+        
+        sample_data_df = pd.DataFrame(
+            {'Sea Level': [1.0, 1.5]},
+            index=data_timestamps_utc
+        )
+        start_epoch_utc = utc_tz.localize(dt(2023, 1, 1, 0, 0, 0))
+
+        time_array, _ = tidal_analysis_module._prepare_tidal_analysis_inputs(
+            sample_data_df,
+            start_epoch_utc
+        )
+        
+        assert time_array[0] == pytest.approx(0.0)
+        assert time_array[1] == pytest.approx(1.0 / 24.0)
+        
     def test_reading_data(self):
         tidal_file = "data/1947ABE.txt"
         
